b275f154045a5629ad20e46a78c71970278298d6
makan1113 <kosarevaeee@mail.ru>
2026-03-02 10:51:00 +0300

    Initial commit

---
+++ src/test/java/ru/makan1/eventmanagement/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.makan1.eventmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
